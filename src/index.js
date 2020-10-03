8a98b1d4a36e3c7ade17eb91fee02b5108c4028f
novikovrabota433 <32838534+novikovrabota433@users.noreply.github.com>
2020-10-04 00:30:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
 exports.min = function min (array) {
-  return 0;
+    if (array.length === 0 || array.isArray === false) {
+        return 0;
+    }
+    var min = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (min > array[i]) min = array[i];
+    }
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array.length === 0 || array.isArray === false) {
+      return 0;
+    }
+    var max = array[0]
+    for (var i = 0; i < array.length; i++) {
+      if (max < array[i]) max = array[i];
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array.length === 0 || array.isArray === false) {
+    return 0;
+  }
+  var sum = 0;
+    for (var i = 0, j = array.length; i < j; i++){
+      sum += array[i];
+    }
+    return sum / array.length;
 }
+
